70ea556220037bcbe4fbe14c07dc7b33f5c2926b
-rebecaaaaa <rchavez@csumb.edu>
2022-11-10 16:27:26 -0800

    BucketBeanz project added

---
+++ BucketBeanz/Pods/Target Support Files/Pods-BucketBeanz-BucketBeanzUITests/Pods-BucketBeanz-BucketBeanzUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BucketBeanz_BucketBeanzUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BucketBeanz_BucketBeanzUITests
+@end
---
+++ BucketBeanz/Pods/Target Support Files/Pods-BucketBeanzTests/Pods-BucketBeanzTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BucketBeanzTests : NSObject
+@end
+@implementation PodsDummy_Pods_BucketBeanzTests
+@end
